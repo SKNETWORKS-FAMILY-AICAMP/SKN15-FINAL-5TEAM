7eafc6e39772b4a6061c2fb4d3240a3ee2fc24a2
none-jun <chicksana28@gmail.com>
2025-11-20 12:32:46 +0900

    버블 문구 수정

--- front/src/utils/bubbleUtils.ts
+++ front/src/utils/bubbleUtils.ts
@@ -1,13 +1,13 @@
 export const getBubbleColor = (count: number) => {
-  if (count > 750) return 'text-green-400';
-  if (count > 500) return 'text-yellow-400';
-  if (count > 250) return 'text-orange-400';
+  if (count > 100) return 'text-green-400';
+  if (count > 50) return 'text-yellow-400';
+  if (count > 10) return 'text-orange-400';
   return 'text-red-400';
 };
 
 export const getBubbleStatus = (count: number) => {
-  if (count > 750) return '풍부함';
-  if (count > 500) return '보통';
-  if (count > 250) return '부족';
+  if (count > 100) return '풍부함';
+  if (count > 50) return '적정';
+  if (count > 10) return '리필 권장';
   return '위험';
 };
